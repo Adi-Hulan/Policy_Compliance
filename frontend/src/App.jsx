1d0feac0732003aa3459aee8054fd3ce2360416e
Adi-Hulan <adithyadrck@gmail.com>
2025-08-23 16:54:41 +0530

    new frontend with js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Demo from "./components/Demo";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <Demo />
-    </>
-  );
+  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
