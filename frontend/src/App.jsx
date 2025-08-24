c8520d7cc907804232bc3fbeba2708f71dc6e2c7
Pamali Kodi <161586406+pvkodi@users.noreply.github.com>
2025-08-24 09:42:16 +0530

    basic chat implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ChatPage from '@/pages/ChatPage'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
