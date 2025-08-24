b54b6988dd05e46fa3e1d89060b9b86e53caf3ee
Adi-Hulan <adithyadrck@gmail.com>
2025-08-24 22:37:18 +0530

    chunking using nltk library because of chunking cap of 100

--- backend/agents/document_processor.py
+++ backend/agents/document_processor.py
@@ -3,22 +3,27 @@ from utils.pdf_parser import extract_text_from_pdf
 from db.connection import get_db
 import os
 import uuid
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
 
 class DocumentProcessor:
     def __init__(self):
         genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
         self.model = "models/embedding-001"
 
-    def chunck_text(self, text, chuck_size=500, overlap=50):
-        words = text.split()
-        chunks=[]
+    def chunk_text(self, text, sentences_per_chunk=5, overlap=2):
+        text = ' '.join(text.split())
+        sentences = sent_tokenize(text)
+        chunks = []
         start = 0
 
-        while start < len(words):
-            end = start + chuck_size
-            chunk = " ".join(words[start:end])
+        while start < len(sentences):
+            end = start + sentences_per_chunk
+            chunk = " ".join(sentences[start:end])
             chunks.append(chunk)
-            start += chuck_size-overlap
+            start += sentences_per_chunk - overlap  # move with overlap
+
         return chunks
 
 
@@ -27,32 +32,36 @@ class DocumentProcessor:
         try:
             # 1. Extract text
             text = extract_text_from_pdf(file_path)
+            print(f"Length of text in characters: {len(text)}")
             if not text.strip():
                 return {"agent": "DocumentProcessor", "status": "error", "result": "No text found in PDF"}
             
-            chunks = self.chunck_text(text)
+            chunks = self.chunk_text(text)
             
             # 3. Save to pgvector
             conn = get_db()
             cur = conn.cursor()
-
+            i=0
 
             for chunk in chunks:
-
+                i=i+1
                 clean_chunk = chunk.replace("\x00", "")
 
-                embedding = genai.embed_content(
+                try:
+                    embedding = genai.embed_content(
                     model=self.model,
                     content=clean_chunk,
                     task_type="retrieval_document"
                 )["embedding"]
-                
+                except Exception as e:
+                    return {"agent": ..., "result": str(e)}
 
                 doc_id = str(uuid.uuid4())
                 cur.execute(
                     "INSERT INTO documents (id, content, embedding) VALUES (%s, %s, %s)",
                     (doc_id, clean_chunk, embedding)
                 )
+                print(f"Inserted chunk {i}")
 
 
             conn.commit()
